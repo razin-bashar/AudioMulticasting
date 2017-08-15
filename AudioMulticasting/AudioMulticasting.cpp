759366668c7c15517a7c53def4143eb697201a1d
Unknown <razinbuet334@gmail.com>
2017-08-15 23:28:01 +0600

    wasapirender added

--- AudioMulticasting/AudioMulticasting.cpp
+++ AudioMulticasting/AudioMulticasting.cpp
@@ -2,10 +2,9 @@
 //
 
 #include "stdafx.h"
-#include <Ws2tcpip.h>
+#include "Common.h"
 #include <initguid.h>
 #include <stdio.h>
-#include <windows.h>
 #include <mmsystem.h>
 #include <mmdeviceapi.h>
 
@@ -209,7 +208,7 @@ int _cdecl wmain(int argc, LPCWSTR argv[])
 	LPCWSTR argv1[4];
 	argv1[1] = L"--device";
 	argv1[2] = L"Speakers (Realtek High Definition Audio)";
-	argv1[3] = L"--int-16";
+	argv1[3] = L"-int-16";
 
 
 	hr = CoInitialize(NULL);
@@ -219,7 +218,7 @@ int _cdecl wmain(int argc, LPCWSTR argv[])
 	}
 	CoUninitializeOnExit cuoe;
 
-	do_everything(4, argv1);
+	do_everything(3, argv1);
 
 	//server.join();
 	//	client.join();
@@ -901,6 +900,221 @@ DWORD WINAPI LoopbackCaptureThreadFunction(LPVOID pContext) {
 	return 0;
 }
 
+#include "WASAPIRenderer.h"
+#include <Strsafe.h>
+bool UseConsoleDevice=false;
+bool UseCommunicationsDevice=false;
+bool UseMultimediaDevice=false;
+LPCWSTR OutputEndpoint = NULL;
+template <class T> void SafeRelease(T **ppT)
+{
+	if (*ppT)
+	{
+		(*ppT)->Release();
+		*ppT = NULL;
+	}
+}
+LPWSTR GetDeviceName(IMMDeviceCollection *DeviceCollection, UINT DeviceIndex)
+{
+	IMMDevice *device;
+	LPWSTR deviceId;
+	HRESULT hr;
+
+	hr = DeviceCollection->Item(DeviceIndex, &device);
+	if (FAILED(hr))
+	{
+		printf("Unable to get device %d: %x\n", DeviceIndex, hr);
+		return NULL;
+	}
+	hr = device->GetId(&deviceId);
+	if (FAILED(hr))
+	{
+		printf("Unable to get device %d id: %x\n", DeviceIndex, hr);
+		return NULL;
+	}
+
+	IPropertyStore *propertyStore;
+	hr = device->OpenPropertyStore(STGM_READ, &propertyStore);
+	SafeRelease(&device);
+	if (FAILED(hr))
+	{
+		printf("Unable to open device %d property store: %x\n", DeviceIndex, hr);
+		return NULL;
+	}
+
+	PROPVARIANT friendlyName;
+	PropVariantInit(&friendlyName);
+	hr = propertyStore->GetValue(PKEY_Device_FriendlyName, &friendlyName);
+	SafeRelease(&propertyStore);
+
+	if (FAILED(hr))
+	{
+		printf("Unable to retrieve friendly name for device %d : %x\n", DeviceIndex, hr);
+		return NULL;
+	}
+
+	wchar_t deviceName[128];
+	hr = StringCbPrintf(deviceName, sizeof(deviceName), L"%s (%s)", friendlyName.vt != VT_LPWSTR ? L"Unknown" : friendlyName.pwszVal, deviceId);
+	if (FAILED(hr))
+	{
+		printf("Unable to format friendly name for device %d : %x\n", DeviceIndex, hr);
+		return NULL;
+	}
+
+	PropVariantClear(&friendlyName);
+	CoTaskMemFree(deviceId);
+
+	wchar_t *returnValue = _wcsdup(deviceName);
+	if (returnValue == NULL)
+	{
+		printf("Unable to allocate buffer for return\n");
+		return NULL;
+	}
+	return returnValue;
+}
+//
+//  Based on the input switches, pick the specified device to use.
+//
+bool PickDevice(IMMDevice **DeviceToUse, bool *IsDefaultDevice, ERole *DefaultDeviceRole)
+{
+	HRESULT hr;
+	bool retValue = true;
+	IMMDeviceEnumerator *deviceEnumerator = NULL;
+	IMMDeviceCollection *deviceCollection = NULL;
+
+	*IsDefaultDevice = false;   // Assume we're not using the default device.
+
+	hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&deviceEnumerator));
+	if (FAILED(hr))
+	{
+		printf("Unable to instantiate device enumerator: %x\n", hr);
+		retValue = false;
+		goto Exit;
+	}
+
+	IMMDevice *device = NULL;
+
+	//
+	//  First off, if none of the console switches was specified, use the console device.
+	//
+	if (!UseConsoleDevice && !UseCommunicationsDevice && !UseMultimediaDevice && OutputEndpoint == NULL)
+	{
+		//
+		//  The user didn't specify an output device, prompt the user for a device and use that.
+		//
+		hr = deviceEnumerator->EnumAudioEndpoints(eRender, DEVICE_STATE_ACTIVE, &deviceCollection);
+		if (FAILED(hr))
+		{
+			printf("Unable to retrieve device collection: %x\n", hr);
+			retValue = false;
+			goto Exit;
+		}
+
+		printf("Select an output device:\n");
+		printf("    0:  Default Console Device\n");
+		printf("    1:  Default Communications Device\n");
+		printf("    2:  Default Multimedia Device\n");
+		UINT deviceCount;
+		hr = deviceCollection->GetCount(&deviceCount);
+		if (FAILED(hr))
+		{
+			printf("Unable to get device collection length: %x\n", hr);
+			retValue = false;
+			goto Exit;
+		}
+		for (UINT i = 0; i < deviceCount; i += 1)
+		{
+			LPWSTR deviceName;
+
+			deviceName = GetDeviceName(deviceCollection, i);
+			if (deviceName == NULL)
+			{
+				retValue = false;
+				goto Exit;
+			}
+			printf("    %d:  %S\n", i + 3, deviceName);
+			free(deviceName);
+		}
+		wchar_t choice[10];
+		_getws_s(choice);   // Note: Using the safe CRT version of _getws.
+
+		long deviceIndex;
+		wchar_t *endPointer;
+
+		deviceIndex = wcstoul(choice, &endPointer, 0);
+		if (deviceIndex == 0 && endPointer == choice)
+		{
+			printf("unrecognized device index: %S\n", choice);
+			retValue = false;
+			goto Exit;
+		}
+		switch (deviceIndex)
+		{
+		case 0:
+			UseConsoleDevice = 1;
+			break;
+		case 1:
+			UseCommunicationsDevice = 1;
+			break;
+		case 2:
+			UseMultimediaDevice = 1;
+			break;
+		default:
+			hr = deviceCollection->Item(deviceIndex - 3, &device);
+			if (FAILED(hr))
+			{
+				printf("Unable to retrieve device %d: %x\n", deviceIndex - 3, hr);
+				retValue = false;
+				goto Exit;
+			}
+			break;
+		}
+	}
+	else if (OutputEndpoint != NULL)
+	{
+		hr = deviceEnumerator->GetDevice(OutputEndpoint, &device);
+		if (FAILED(hr))
+		{
+			printf("Unable to get endpoint for endpoint %S: %x\n", OutputEndpoint, hr);
+			retValue = false;
+			goto Exit;
+		}
+	}
+
+	if (device == NULL)
+	{
+		ERole deviceRole = eConsole;    // Assume we're using the console role.
+		if (UseConsoleDevice)
+		{
+			deviceRole = eConsole;
+		}
+		else if (UseCommunicationsDevice)
+		{
+			deviceRole = eCommunications;
+		}
+		else if (UseMultimediaDevice)
+		{
+			deviceRole = eMultimedia;
+		}
+		hr = deviceEnumerator->GetDefaultAudioEndpoint(eRender, deviceRole, &device);
+		if (FAILED(hr))
+		{
+			printf("Unable to get default device for role %d: %x\n", deviceRole, hr);
+			retValue = false;
+			goto Exit;
+		}
+		*IsDefaultDevice = true;
+		*DefaultDeviceRole = deviceRole;
+	}
+
+	*DeviceToUse = device;
+	retValue = true;
+Exit:
+	SafeRelease(&deviceCollection);
+	SafeRelease(&deviceEnumerator);
+
+	return retValue;
+}
 
 HRESULT LoopbackCapture(
 	IMMDevice *pMMDevice,
@@ -910,8 +1124,47 @@ HRESULT LoopbackCapture(
 	HANDLE hStopEvent,
 	PUINT32 pnFrames
 	) {
+	bool isDefaultDevice;
+	ERole role;
 
+	if (!PickDevice(&pMMDevice, &isDefaultDevice, &role))
+	{
+		
+		exit;
+	}
 
+	CWASAPIRenderer *renderer = new (std::nothrow) CWASAPIRenderer(pMMDevice, isDefaultDevice, role);
+	if (renderer == NULL)
+	{
+		printf("Unable to allocate renderer\n");
+		return -1;
+	}
+
+	//9876
+	RenderBuffer *renderQueue =NULL;
+	RenderBuffer **currentBufferTail = &renderQueue;
+	int TargetLatency = 30;
+	if (renderer->Initialize(TargetLatency))
+	{
+		//
+		//  We've initialized the renderer.  Once we've done that, we know some information about the
+		//  mix format and we can allocate the buffer that we're going to render.
+		//
+		//
+		//  The buffer is going to contain "TargetDuration" seconds worth of PCM data.  That means 
+		//  we're going to have TargetDuration*samples/second frames multiplied by the frame size.
+		//
+	//	UINT32 renderBufferSizeInBytes = (renderer->BufferSizePerPeriod()  * renderer->FrameSize());
+	//	size_t renderDataLength = (renderer->SamplesPerSecond() * TargetDurationInSec * renderer->FrameSize()) + (renderBufferSizeInBytes - 1);
+	//	size_t renderBufferCount = renderDataLength / (renderBufferSizeInBytes);
+		//
+		//  Render buffer queue. Because we need to insert each buffer at the end of the linked list instead of at the head, 
+		//  we keep a pointer to a pointer to the variable which holds the tail of the current list in currentBufferTail.
+		//
+		RenderBuffer *renderQueue = NULL;
+		RenderBuffer **currentBufferTail = &renderQueue;
+	}
+	
 	HRESULT hr;
 	// activate an IAudioClient
 	IAudioClient *pAudioClient;
@@ -1142,8 +1395,24 @@ HRESULT LoopbackCapture(
 
 			int str = strlen(reinterpret_cast<PCHAR>(pData));
 
+			//12345
 
-
+			RenderBuffer *renderBuffer = new (std::nothrow) RenderBuffer();
+			if (renderBuffer == NULL)
+			{
+				printf("Unable to allocate render buffer\n");
+				return -1;
+			}
+			renderBuffer->_BufferLength = lBytesWritten;
+			renderBuffer->_Buffer = new (std::nothrow) BYTE[lBytesWritten];
+			if (renderBuffer->_Buffer == NULL)
+			{
+				printf("Unable to allocate render buffer\n");
+				return -1;
+			}
+			memcpy(renderBuffer->_Buffer, (BYTE*)reinterpret_cast<PCHAR>(pData), lBytesWritten);
+			*currentBufferTail = renderBuffer;
+			currentBufferTail = &renderBuffer->_Next;
 
 
 
@@ -1184,6 +1453,21 @@ HRESULT LoopbackCapture(
 			return E_UNEXPECTED;
 		}
 	} // capture loop
+
+
+	if (renderer->Start(renderQueue))
+	{
+		do
+		{
+			printf(".");
+			Sleep(1000);
+		} while (true);
+		printf("\n");
+
+		renderer->Stop();
+		renderer->Shutdown();
+		SafeRelease(&renderer);
+	}
 
 	hr = FinishWaveFile(hFile, &ckData, &ckRIFF);
 	if (FAILED(hr)) {
